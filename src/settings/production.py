c0fb84105f2c51d0ab8da4f3d7bb7cdad8b4c1f2
Chris Stenkamp <cstenkamp@gmail.com>
2021-06-28 20:24:51 +0200

    1-command-deployment works

--- src/settings/production.py
+++ src/settings/production.py
@@ -5,7 +5,7 @@ from settings.settings_base import BASE_DIR
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
