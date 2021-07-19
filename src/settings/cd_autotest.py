8170b7f5fea590a394fc983bc5088a634a40ad9e
Chris Stenkamp <cstenkamp@gmail.com>
2021-07-19 17:24:04 +0200

    cd fix

--- src/settings/cd_autotest.py
+++ src/settings/cd_autotest.py
@@ -6,5 +6,5 @@ from settings.settings_base import BASE_DIR
 ALLOWED_HOSTS = ["127.0.0.1"]
 DEBUG = False
 
-STATIC_ROOT = os.path.join(BASE_DIR, "siddata_server", "django_data", "collected_static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "siddata_server", "django_data", "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "django_data", "collected_static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "django_data", "media")
